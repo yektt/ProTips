dfc091bf06d5084e942436d79eed94547b2eb83c
yektt <yektaturan.93@gmail.com>
2020-11-10 11:32:56 +0100

    test: first test for system-comments test
    -checking pluralized or singularized comments on tips_card

--- pro_tips/test/system/comments_test.rb
+++ pro_tips/test/system/comments_test.rb
@@ -1,9 +1,22 @@
 require "application_system_test_case"
 
 class CommentsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit comments_url
-  #
-  #   assert_selector "h1", text: "Comment"
-  # end
+
+  test 'Checking comment.s on a tip if it is pluralized or singularized' do
+    user = User.new email:'user@epfl.ch', name:'Kyle'
+    user.save!
+    tip = Tip.new title:'tip title', body:'tip body', user_id:user.id
+    tip.save!
+    comment1 = Comment.new body:'comment1 body', user_id:user.id, tip_id:tip.id
+    comment1.save!
+
+    visit(tips_path(tip))
+    assert page.has_content?("1 comment on this tip")
+
+    comment2 = Comment.new body:'comment2 body', user_id:user.id, tip_id:tip.id
+    comment2.save!
+
+    visit(tips_path(tip))
+    assert page.has_content?("2 comments on this tip")
+  end
 end

bc5ee6d8338f5e833f5fd71cd9fae4599c18071f
yektt <yektaturan.93@gmail.com>
2020-11-10 11:34:13 +0100

    test: checking user comments on user_page if it's pluralized or singularized

--- pro_tips/test/system/comments_test.rb
+++ pro_tips/test/system/comments_test.rb
@@ -19,4 +19,22 @@ class CommentsTest < ApplicationSystemTestCase
     visit(tips_path(tip))
     assert page.has_content?("2 comments on this tip")
   end
+
+  test 'checking user comment.s if it is pluralized or singularized' do
+    user = User.new email:'user@epfl.ch', name:'Kyle'
+    user.save!
+    tip = Tip.new title:'tip title', body:'tip body', user_id:user.id
+    tip.save!
+    comment1 = Comment.new body:'comment1 body', user_id:user.id, tip_id:tip.id
+    comment1.save!
+
+    visit(user_path(user))
+    assert page.has_content?("and made 1 comment")
+
+    comment2 = Comment.new body:'comment2 body', user_id:user.id, tip_id:tip.id
+    comment2.save!
+    
+    visit(user_path(user))
+    assert page.has_content?("and made 2 comments")
+  end
 end

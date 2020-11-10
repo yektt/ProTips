7f224d7ecae1fd51a8be654abfe8909bfcd2671f
yektt <yektaturan.93@gmail.com>
2020-11-10 11:35:39 +0100

    test: first test for system-tips tests
    -checking pluralized or singularized tips on user_card

--- pro_tips/test/system/tips_test.rb
+++ pro_tips/test/system/tips_test.rb
@@ -1,9 +1,20 @@
 require "application_system_test_case"
 
 class TipsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit tips_url
-  #
-  #   assert_selector "h1", text: "Tip"
-  # end
+  test 'Checking tip if it is pluralized or singularized' do
+    user = User.new email:'user@epfl.ch', name:'Kyle'
+    user.save!
+    tip1 = Tip.new title:'tip title', body:'tip body', user_id:user.id
+    tip1.save!
+
+    visit(user_path(user))
+    assert page.has_content?("Has created 1 tip")
+
+    tip2 = Tip.new title:'tip title', body:'tip body', user_id:user.id
+    tip2.save!
+
+    visit(user_path(user))
+    assert page.has_content?("Has created 2 tips")
+  end
 end
+

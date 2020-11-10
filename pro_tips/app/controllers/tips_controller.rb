bf8d135e2baa8b99670ec3a1bc6874d231e76272
yektt <yektaturan.93@gmail.com>
2020-11-10 15:06:48 +0100

    created search scopes for searching tips

--- pro_tips/app/controllers/tips_controller.rb
+++ pro_tips/app/controllers/tips_controller.rb
@@ -2,8 +2,8 @@ class TipsController < ApplicationController
   before_action :set_tip, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tips = Tip.where('title LIKE ?', "%#{params[:q]}%").or(
-      Tip.where('body LIKE ?', "%#{params[:q]}%"))
+    @search_term = params[:q]
+    @tips = Tip.search(@search_term)
   end
 
   def show
--- pro_tips/app/models/tip.rb
+++ pro_tips/app/models/tip.rb
@@ -7,4 +7,9 @@ class Tip < ApplicationRecord
   validates :title, presence: true,
                     length: { maximum: 100 }
   validates :body,  presence: true
+
+  scope :title_contains, -> (term) { where('title LIKE ?', "%#{term}%") }
+  scope :body_contains,  -> (term) { where('body LIKE ?', "%#{term}%") }
+  scope :search,         ->(search_term) { title_contains(search_term).or(body_contains(search_term)) }
+
 end

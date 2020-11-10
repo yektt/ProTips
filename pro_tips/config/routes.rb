78282f06ef7c559f4d70aa0d9ac05d5fce813f94
yektt <yektaturan.93@gmail.com>
2020-11-10 17:36:45 +0100

    Revert "add a mechanism as URI paramater for selecting wich locale file will be rendered"
    This reverts commit 3e997ea5e18ffdf23ac3cf745f686980956c074e.

--- pro_tips/config/routes.rb
+++ pro_tips/config/routes.rb
@@ -1,25 +1,21 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root to: 'home#index'
-  
-  scope '/:locale' do
-    get 'sessions/new'
 
-    root to: 'home#index'
+  get  '/account', to: 'account#edit'
+  post '/account', to: 'account#update'
 
-    get  '/account', to: 'account#edit'
-    post '/account', to: 'account#update'
-
-    namespace :account do
-      resources :tips, only: [:index]
-    end
-
-    resources :tips do
-      resources :comments, only: [:create]
-    end
-
-    resources :sessions, only: [:new, :create, :destroy]
-
-    resources :users
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :account do
+    resources :tips, only: [:index]
   end
+
+  resources :tips do
+    resources :comments, only: [:create]
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

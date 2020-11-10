c288e014af7a0e455ffc7e587bfbbcee5b0c088a
yektt <yektaturan.93@gmail.com>
2020-11-10 17:38:16 +0100

    add URI paramater for selecting which locale will be rendered

--- pro_tips/config/routes.rb
+++ pro_tips/config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   root to: 'home#index'
+  
+  scope '/:locale' do
+    get 'sessions/new'
 
-  get  '/account', to: 'account#edit'
-  post '/account', to: 'account#update'
+    root to: 'home#index'
 
-  namespace :account do
-    resources :tips, only: [:index]
+    get  '/account', to: 'account#edit'
+    post '/account', to: 'account#update'
+
+    namespace :account do
+      resources :tips, only: [:index]
+    end
+
+    resources :tips do
+      resources :comments, only: [:create]
+    end
+
+    resources :sessions, only: [:new, :create, :destroy]
+
+    resources :users
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
-
-  resources :tips do
-    resources :comments, only: [:create]
-  end
-
-  resources :sessions, only: [:new, :create, :destroy]
-
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

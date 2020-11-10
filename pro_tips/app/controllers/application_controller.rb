3e997ea5e18ffdf23ac3cf745f686980956c074e
yektt <yektaturan.93@gmail.com>
2020-11-10 17:36:23 +0100

    add a mechanism as URI paramater for selecting wich locale file will be rendered

--- pro_tips/app/controllers/application_controller.rb
+++ pro_tips/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end

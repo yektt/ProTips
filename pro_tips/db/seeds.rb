66b718e10e61ca27a108c437b79496a7f73ae5c6
yektt <yektaturan.93@gmail.com>
2020-11-10 12:20:47 +0100

    db: seed db has expended with 5 comments

--- pro_tips/db/seeds.rb
+++ pro_tips/db/seeds.rb
@@ -24,32 +24,31 @@ Tip.create!(title: 'Use #name on a class in Ruby',
             body:  'Using Class.name returns a String representing the name of a class!  E.g. Integer.name returns "Integer" and ApplicationController.name returns "ApplicationController"',
             user:  florian)
 Tip.create!(title: 'Getting a substring in Ruby',
-            body: 'Use String#[start,length] to return the substring of a String, e.g. "Hello, world"[7,5] => "world"',
+            body:  'Use String#[start,length] to return the substring of a String, e.g. "Hello, world"[7,5] => "world"',
             user:  florian)
 Tip.create!(title: 'Going backwards in an Array',
             body:  "It's possible to use negative numbers as backwards indexes in Arrays e.g. [1,2,3,4][-1] => 4",
             user:  florian)
-
 Tip.create!(title: 'Using ERB in CSS and Sass files',
             body:  "Did you know that you can define CSS assets as ERB files, just like .html.erb and .js.erb views?  By using .scss.erb as the extension of a file, the asset pipeline will first process the file as ERB and evaluate Ruby expressions.  Then it will treat the file as Sass and compile it to CSS!",
             user:  sara)
 Tip.create!(title: 'Substitute a value in a String',
             body:  "String#gsub can be used to replace any substring with another substring in a String, e.g. 'Learn about :language'.gsub(':language', 'JavaScript') => \"Learn about JavaScript\"",
             user:  sara)
-            Tip.create!(title: 'Multi-line Strings using %Q',
-                        body:  percent_q_string,
-                        user:  sara)
+Tip.create!(title: 'Multi-line Strings using %Q',
+            body:  percent_q_string,
+            user:  sara)
 Tip.create!(title: 'Using underscores in numbers',
             body:  "It's pretty cool that underscores can be used to define numbers in Ruby, such as 16_453 or 12_345_678.  It can make big numbers much more readable!",
             user:  sara)
 Tip.create!(title: 'Calculate a number to a power',
-            body:  'Use ** to calculate the number to a particular power, e.g. 2**5 => 32', user: sara)
-
+            body:  'Use ** to calculate the number to a particular power, e.g. 2**5 => 32', 
+            user:   sara)
 Tip.create!(title: 'Using rails:secret',
             body:  'Use rails:secret to generate a long secret key for any purpose, not just for a value to use as SECRET_KEY_BASE.',
             user:  linda)
 Tip.create!(title: 'Try using grep with log files',
-            body:  'grep is a terminal program that can be used to search log files for a particular pattern.  It takes a search term as a regular expression and is therefore very powerful.  Learning about regular expressions is very useful for other tasks as well; they are often used in Rails model validations, route definitions and in JavaScript',
+            body:  'grep is a terminal program that can be used to search log files for a particular pattern.  It takes a search term as a regular expression and is therefore very powerful. Learning about regular expressions is very useful for other tasks as well; they are often used in Rails model validations, route definitions and in JavaScript',
             user:  linda)
 Tip.create!(title: 'Organize CSS rules in Sass files well',
             body:  "It's worth taking the time to organize CSS rules in various Sass files, keeping related rules together.  It helps someone new to a project know where to look for particular rules.",
@@ -57,3 +56,19 @@ Tip.create!(title: 'Organize CSS rules in Sass files well',
 Tip.create!(title: 'Just experiment!',
             body:  "The great thing about writing software is that it's easy to try things out and change them before submitting it for a production deployment.  I've found that taking a break and coming back to a difficult programming problem really helps too.",
             user:  linda)
+
+Comment.create!(body:   'Thank you! It will help to develop my new program much faster!',
+                tip_id: 2,
+                user:   sara)
+Comment.create!(body:   'I have spent days on this! I cannot believe that it is this easy to do!',
+                tip_id: 2,
+                user:   linda)
+Comment.create!(body:   'Can you explain more about it? I am curious where can I use secret value other than SECRET_KEY_BASE.',
+                tip_id: 9,
+                user:   sara)
+Comment.create!(body:   'I was quite sick of writing so long lines. Thanks for the information!',
+                tip_id: 6,
+                user:   florian)
+Comment.create!(body:   "You're welcome. Glad to help! :)",
+                tip_id: 6,
+                user:   sara)

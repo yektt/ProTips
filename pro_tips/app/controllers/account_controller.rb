4da8e659b63b049cce5666c11eb2799fc149d145
yektt <yektaturan.93@gmail.com>
2020-11-11 23:09:34 +0100

    refactor: moved 'ensure_authenticated' helper one class up to use in all controllers

--- pro_tips/app/controllers/account_controller.rb
+++ pro_tips/app/controllers/account_controller.rb
@@ -9,7 +9,4 @@ class AccountController < ApplicationController
     redirect_to account_path
   end
 
-  def ensure_authenticated
-    redirect_to sessions_new_path unless(logged_in?) 
-  end
 end
--- pro_tips/app/controllers/application_controller.rb
+++ pro_tips/app/controllers/application_controller.rb
@@ -20,4 +20,8 @@ class ApplicationController < ActionController::Base
   def current_user
     User.find(session[:user_id])
   end
+
+  def ensure_authenticated
+    redirect_to sessions_new_path unless(logged_in?)
+  end
 end

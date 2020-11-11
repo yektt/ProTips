b6a6ea1d41f5f92f7f297ec83f84beb74bccb7ff
yektt <yektaturan.93@gmail.com>
2020-11-11 22:15:12 +0100

    write helper method to allowed only logged in users to use AccountController actions

--- pro_tips/app/controllers/account_controller.rb
+++ pro_tips/app/controllers/account_controller.rb
@@ -1,9 +1,15 @@
 class AccountController < ApplicationController
+  before_action :ensure_authenticated
+  
   def edit
     @user = User.first
   end
 
   def update
     redirect_to account_path
+  end
+
+  def ensure_authenticated
+    redirect_to sessions_new_path unless(logged_in?) 
   end
 end

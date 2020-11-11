b1bc1d5998712837d0f7c705d94ba8a117cbaa72
yektt <yektaturan.93@gmail.com>
2020-11-11 16:30:37 +0100

    helper: created a helper method to check if user logged in or not
    -up to answer, holding the logged in user as session user

--- pro_tips/app/controllers/application_controller.rb
+++ pro_tips/app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :logged_in?
+
   before_action :set_locale
 
   def default_url_options
@@ -9,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def logged_in? 
+    session[:user_id].present?
   end
 end

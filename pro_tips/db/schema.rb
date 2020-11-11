c42572d1e757ced071c024bf3868b5a44fa935f3
yektt <yektaturan.93@gmail.com>
2020-11-11 09:54:29 +0100

    create a string for User model to store secured password

--- pro_tips/db/schema.rb
+++ pro_tips/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125102303) do
+ActiveRecord::Schema.define(version: 20201111085318) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20180125102303) do
     t.string "avatar_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

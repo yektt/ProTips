b7b09ce781e3969a5c48f3df82aa6e7f2cb0dfd4
yektt <yektaturan.93@gmail.com>
2020-11-11 15:07:22 +0100

    feat: added authentication

--- pro_tips/app/controllers/sessions_controller.rb
+++ pro_tips/app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email].downcase)
+
+    if(user.present? && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      redirect_to account_tips_path
+    else
+      flash[:alert] = "Email or password is invalid.  Please try again"
+      render 'new'
+    end
   end
 
   def destroy

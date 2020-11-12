7f47de76230ea27581eb438aebe5d9e9df3b25f9
yektt <yektaturan.93@gmail.com>
2020-11-12 15:14:03 +0100

    if user not logged in, cannot edit tips
    put if statements for not to show 'edit' button in all pages

--- pro_tips/app/helpers/roles_helper.rb
+++ pro_tips/app/helpers/roles_helper.rb
@@ -1,8 +1,11 @@
 module RolesHelper
   def can_edit?(tip)
+    return false unless logged_in?
+
     case(current_user.role)
     when 'admin'      then true
     when 'registered' then tip.user == current_user
     else false
+    end
   end
 end

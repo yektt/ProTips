4ec33abe4aa76eba1caaa662e74d7e61b69f881f
yektt <yektaturan.93@gmail.com>
2020-11-12 10:39:59 +0100

    refactor: implement lazy loading for current_user

--- pro_tips/app/controllers/application_controller.rb
+++ pro_tips/app/controllers/application_controller.rb
@@ -18,7 +18,11 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    User.find(session[:user_id])
+    if(@current_user.present?)
+      return @current_user
+    end
+
+    @current_user = User.find(session[:user_id])
   end
 
   def ensure_authenticated

30665414e17c10413c7405a3b7bc7030926bba9a
yektt <yektaturan.93@gmail.com>
2020-11-12 22:30:54 +0100

    created 'avatar' attribute for user
    -we will be using this attribute for carrierwave

--- pro_tips/db/schema.rb
+++ pro_tips/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201112101122) do
+ActiveRecord::Schema.define(version: 20201112212931) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20201112101122) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "role"
+    t.string "avatar"
   end
 
 end

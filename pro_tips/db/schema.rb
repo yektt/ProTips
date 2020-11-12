8afc24ee0e0f92677e3e8f57ed8f021447460409
yektt <yektaturan.93@gmail.com>
2020-11-12 11:13:20 +0100

    'role' attribute added to user

--- pro_tips/db/schema.rb
+++ pro_tips/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201111085318) do
+ActiveRecord::Schema.define(version: 20201112101122) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20201111085318) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "role"
   end
 
 end

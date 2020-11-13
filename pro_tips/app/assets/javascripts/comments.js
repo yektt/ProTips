f8f5df63dbe414d3bc83fc288b9702d29214cde2
yektt <yektaturan.93@gmail.com>
2020-11-13 20:58:04 +0100

    feat: adding comments dynamically

--- pro_tips/app/assets/javascripts/comments.js
+++ pro_tips/app/assets/javascripts/comments.js
@@ -1,2 +1,37 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+Comments = {};
+
+Comments.displayComment = function(comment) {
+  
+  var commentParagraph = document.createElement('p');
+  commentParagraph.className = 'mb-0';
+  commentParagraph.appendChild(document.createTextNode(comment.body));
+
+  var commenterAvatar = document.createElement('img');
+  commenterAvatar.className = 'avatar-xs';
+  commenterAvatar.src = comment.commenter_avatar;
+  commenterAvatar.alt = comment.commenter;
+
+  var commentLink = document.createElement('a');
+  commentLink.href = comment.link;
+  commentLink.appendChild(commenterAvatar);
+  commentLink.appendChild(document.createTextNode(" " + comment.commenter));
+
+  var commentFooter = document.createElement('footer');
+  commentFooter.className = 'blockquote-footer';
+  commentFooter.appendChild(document.createTextNode('Added by '));
+  commentFooter.appendChild(commentLink);
+  commentFooter.appendChild(document.createTextNode(' on ' + comment.time));
+
+  var commentBlockquote = document.createElement('blockquote');
+  commentBlockquote.className = 'blockquote';
+  commentBlockquote.appendChild(commentParagraph);
+  commentBlockquote.appendChild(commentFooter);
+}
+
+Comments.addComment = function(comment) {
+  var newComment, comments;
+
+  newComment = Comments.displayComment(comment);
+  comments = document.getElementById('commentList');
+  comments.insertBefore(newComment, comments.lastChild);
+}

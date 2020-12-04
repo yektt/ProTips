a2e743aa635b876ae96cc1a736f2b3c30aa8aafe
yektt <yektaturan.93@gmail.com>
2020-12-04 08:53:26 +0100

    when user signed up, will be automatically login in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
         # redirect_to().  It's a caonvenient way of setting a flash notice or
         # alert without referencing the flash Hash explicitly.
         format.html { redirect_to @user, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
       else
         format.html { render :new }
       end

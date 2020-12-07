89a6180ba514bfca105f60ebeb1f0b21250eec5c
yektt <yektaturan.93@gmail.com>
2020-12-07 11:21:22 +0100

    test: wrote models test for comment model
    -presence of the body of comment
    -cascading save

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'presence of the body' do
+    user = User.new email:'user@epfl.ch', name:'Kyle', password:'password'
+    user.save!
+    tip = Tip.new title:'tip title', body:'tip body', user:user
+    tip.save!
+
+    comment = Comment.new user: user, tip: tip
+
+    refute comment.valid?
+  end
+  
+  test 'cascading save' do
+    user = User.new email:'user@epfl.ch', name:'Kyle', password:'password'
+    user.save!
+    tip = Tip.new title:'tip title', body:'tip body', user:user
+    tip.save!
+
+    comment = Comment.new body: "It is a comment!", user: user, tip: tip
+    tip.comments << comment
+    tip.save
+  
+    assert_equal comment, Comment.first
+  end 
 end

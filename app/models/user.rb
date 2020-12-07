1eb970a1cac9de15a21c5822c866bdbc65d5f584
yektt <yektaturan.93@gmail.com>
2020-12-07 12:33:33 +0100

    delete all related items when the user will be deleted
    as:
    -comments
    -tips

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :tips
-  has_many :comments
+  has_many :tips, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   has_and_belongs_to_many :favourites, class_name: Tip.name
 

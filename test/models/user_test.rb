07dcb0f6073ecd881ed060d6706eb3bffa036ae8
yektt <yektaturan.93@gmail.com>
2020-12-07 11:25:51 +0100

    test: wrote model test for user model
    -if email is lowercased before validation
    -user cannot be exist without password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'email is lowercase before validation' do 
+    user = User.new email:'nEW@EpFl.ch', password: 'password'
+    user.save!
+
+    assert_equal user.email, 'new@epfl.ch'
+  end
+
+  test 'user cannot be exist without password' do 
+    user = User.new email:'new@epfl.ch'
+    
+    refute user.valid?
+  end
 end
